e18c3985325e19fd79f36fcb0d5dd6ac2e56aa39
Stephane Ds <92688358+Naneeee@users.noreply.github.com>
2022-06-29 12:51:16 +0200

    start of restaurant routes

--- routes/index.routes.js
+++ routes/index.routes.js
@@ -1,11 +1,15 @@
-const router = require("express").Router();
-
-router.get("/", (req, res, next) => {
-  res.json("All good in here");
-});
+const express = require("express")
+const router = express.Router()
 
 // You put the next routes here 👇
 // example: router.use("/auth", authRoutes)
-router.use("/auth", require("./user.routes"));
-router.use("/reservation", require("./Reservation.routes"));
-module.exports = router;
+router.use("/auth", require("./user.routes"))
+router.use("/reservation", require("./Reservation.routes"))
+
+/* GET /
+It's useful to have a route which we can request to check the API is running.
+Sometimes this is at /health or /check.
+*/
+router.get("/", (req, res, next) => res.json({ success: true }))
+
+module.exports = router
---
+++ routes/restaurant.js
@@ -0,0 +1,55 @@
+const express = require("express")
+const router = express.Router()
+const { default: mongoose } = require("mongoose")
+
+// require the restaurant model
+const restaurantModel = require("../models/Restaurant.model")
+
+// List of all the restaurants
+router.get("/restaurants", async (req, res, next) => {
+  try {
+    const allRestaurant = await restaurantModel.find()
+    res.status(200).json(allRestaurant)
+  } catch (err) {
+    next(err)
+  }
+})
+
+// Add a new restaurant
+router.post("/restaurants", async (req, res, next) => {
+  try {
+    const createNew = await restaurantModel.create(req.body)
+    res.status(201).json(newRestaurantCreated)
+  } catch (err) {
+    res.status(400).json("restaurant not created")
+  }
+})
+
+//  Update a restaurant
+router.patch("/restaurants/:id", async (req, res, next) => {
+  try {
+    const restsaurantId = req.params.id
+    const updateRestaurant = await droneModel.findByIdAndUpdate(
+      req.params.id,
+      req.body,
+      {
+        new: true,
+      }
+    )
+    res.status(200).json(updatedRestaurant)
+  } catch (err) {
+    next(err)
+  }
+})
+
+// Delete the restaurant
+router.delete("/restaurants/:id", async (req, res, next) => {
+  try {
+    const deletedrestaurant = await Drone.findByIdAndDelete(req.params.id)
+    res.json({ message: `you deleted ${deletedRestaurant}` })
+  } catch (err) {
+    next(err)
+  }
+})
+
+module.exports = router
