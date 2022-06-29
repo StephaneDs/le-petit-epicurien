c63f1ebd2f2178b795a4fef291a77dedec5320cd
Stephane Ds <92688358+Naneeee@users.noreply.github.com>
2022-06-29 12:58:34 +0200

    fixing naming of route

--- routes/restaurant.routes.js
+++ routes/restaurant.routes.js
@@ -3,12 +3,12 @@ const router = express.Router()
 const { default: mongoose } = require("mongoose")
 
 // require the restaurant model
-const restaurantModel = require("../models/Restaurant.model")
+const Restaurant = require("../models/Restaurant.model")
 
 // List of all the restaurants
 router.get("/restaurants", async (req, res, next) => {
   try {
-    const allRestaurant = await restaurantModel.find()
+    const allRestaurant = await Restaurant.find()
     res.status(200).json(allRestaurant)
   } catch (err) {
     next(err)
@@ -18,7 +18,7 @@ router.get("/restaurants", async (req, res, next) => {
 // Add a new restaurant
 router.post("/restaurants", async (req, res, next) => {
   try {
-    const createNew = await restaurantModel.create(req.body)
+    const createNew = await Restaurant.create(req.body)
     res.status(201).json(newRestaurantCreated)
   } catch (err) {
     res.status(400).json("restaurant not created")
