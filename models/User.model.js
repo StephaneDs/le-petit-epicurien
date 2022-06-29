fe69e75e649680d3bd6e3a1b875deb60dbefd17d
caetanolmsouza <caetanomartins@hotmail.com>
2022-06-29 12:14:42 +0200

    User model, user routes, and middleware adds

--- models/User.model.js
+++ models/User.model.js
@@ -1,20 +1,10 @@
-const { Schema, model } = require("mongoose");
+const mongoose = require("mongoose");
+const { Schema, model } = mongoose;
 
-// TODO: Please make sure you edit the user model to whatever makes sense in this case
-const userSchema = new Schema(
-  {
-    username: {
-      type: String,
-      // unique: true -> Ideally, should be unique, but its up to you
-    },
-    password: String,
-  },
-  {
-    // this second object adds extra properties: `createdAt` and `updatedAt`
-    timestamps: true,
-  }
-);
+const userSchema = new Schema({
+  email: { type: String, unique: true, required: true },
+  password: { type: String, required: true },
+  name: { type: String, required: true },
+});
 
-const User = model("User", userSchema);
-
-module.exports = User;
+module.exports = model("User", userSchema);
