b659131e66b9af1ac5f62fa40d6487caf5b06d65
caetanolmsouza <caetanomartins@hotmail.com>
2022-07-06 15:28:08 +0200

    signup button and redirection fixed

--- models/User.model.js
+++ models/User.model.js
@@ -1,10 +1,10 @@
-const mongoose = require("mongoose");
-const { Schema, model } = mongoose;
+const mongoose = require('mongoose')
+const { Schema, model } = mongoose
 
 const userSchema = new Schema({
   email: { type: String, unique: true, required: true },
   password: { type: String, required: true },
-  name: { type: String, required: true },
-});
+  username: { type: String, required: true },
+})
 
-module.exports = model("User", userSchema);
+module.exports = model('User', userSchema)
